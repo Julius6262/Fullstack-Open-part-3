adf1bcd4adff73ae608c27040501fdca350841e3
Martin <jo.ju.martin@gmail.com>
2024-04-15 11:38:27 +0200

    3.4: Phonebook backend step 4 exercise solution

--- index.js
+++ index.js
@@ -47,35 +47,33 @@ app.delete('/api/persons/:id', (request, response) => {
 });
 
 
-/*
 const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-    return maxId + 1
+  const maxId = 1000000;
+  return Math.floor(Math.random() * maxId);
+}
+
+  
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'name or number missing' 
+    })
   }
-  */
- /*
-  app.post('/api/notes', (request, response) => {
-    const body = request.body
-  
-    if (!body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
-    }
-  
-    const note = {
-      content: body.content,
-      important: Boolean(body.important) || false,
-      id: generateId(),
-    }
-  
-    notes = notes.concat(note)
-  
-    response.json(note)
-  })
-*/
+
+  const pb = {
+    id: generateId(),
+    name: body.name, //name
+    number: body.number
+    
+  }
+
+  phonebook = phonebook.concat(pb)
+
+  response.json(pb)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
